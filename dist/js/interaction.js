3cf7f7a3e0769d8fed22d6d814e0299029791419
sam <samface@gmail.com>
2019-04-09 23:45:03 -0700

    all kinds of fixes

--- dist/js/interaction.js
+++ dist/js/interaction.js
@@ -8,10 +8,10 @@
 
 console.log("interaction.js loaded")
 
-$(document).ready((function() {
+// $(document).ready(function() {
   // console.log("hi i'm ready")
   // var bugger = $("#bugger")
 
   // bugger.html(window.innerWidth)
 
-}))
+// })
--- src/js/interaction.js
+++ src/js/interaction.js
@@ -1,9 +1,9 @@
 console.log("interaction.js loaded")
 
-$(document).ready(function() {
+// $(document).ready(function() {
   // console.log("hi i'm ready")
   // var bugger = $("#bugger")
 
   // bugger.html(window.innerWidth)
 
-})
+// })
